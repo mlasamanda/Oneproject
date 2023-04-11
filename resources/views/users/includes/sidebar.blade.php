93e0a2d46d70da60df800c3d68d609d9f22398fe
sayi mlasa <sayimlasa2021@gmail.com>
2023-04-11 10:44:46 +0300

    commit first

---
+++ resources/views/users/includes/sidebar.blade.php
@@ -0,0 +1,93 @@
+<!-- Brand Logo -->
+
+<a href="#" class="brand-link">
+      <img src="{{asset('assets/dist/img/logo.jpg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light"><b>Book Store</b></span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Admin</a>
+        </div>
+      </div> -->
+      <!-- Sidebar Menu -->
+
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="{{route('home')}}" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+
+              </p>
+            </a>
+           </li>
+           
+           <li class="nav-item has-treeview">
+            <a href="" class="nav-link">
+              <i class="fas fa-angle-left right"></i>
+              <p>
+                Manangement
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+            <li class="nav-item">
+                <a href="{{ route('users.index') }}" class="nav-link">
+                  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fas fa-user fa-fw"></i>
+                  <p>Manage Users</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('roles.index') }}" class="nav-link">
+                   &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fas fa-clock"></i>
+                  <p>Manage Role</p>
+                </a>
+            </li>
+              <li class="nav-item">
+                <a href="{{ route('products.index') }}" class="nav-link">
+                   &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<i class="fa fa-cloud"></i>
+                  <p>Product Role</p>
+                </a>
+               </li>  
+             
+          </ul>
+          <li class="nav-item has-treeview">
+            <a href=" " class="nav-link">
+              <i class="nav-icon fas fa-key"></i>
+              <p>
+                change Password
+              </p>
+            </a>
+          </li>
+
+
+
+          <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-power-off"></i>
+              <p>
+                {{ __('Logout') }}
+              </p>
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+             </form>
+          </li>
+
+
+        </ul>
+      </nav>
+
+    </div>
+    <!-- /.sidebar -->
